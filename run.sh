f6fc7a74a9ace45d25e3b0f000968e553ab652a3
Mario Basic <mbasic99@gmail.com>
2026-01-06 00:38:02 +0100

    .

--- run.sh
+++ run.sh
@@ -6,12 +6,6 @@ if ! command -v podman &> /dev/null; then
     exit 1
 fi
 
-# check if nexus server image exists
-# if ! podman image exists localhost/nexus-server:nexus; then
-#     echo "nexus-server image does not exist, build it first"
-#     exit 1
-# fi
-
 mkdir -p /tn_devops/nexus
 
 podman run -d -p 18081:8081 \
@@ -26,15 +20,3 @@ if [ $? -eq 0 ]; then
 else
     echo "Error: Failed to start the Nexus server container."
 fi
-
-# # Start container on boot
-# podman generate systemd --new --name nexus > /usr/lib/systemd/system/nexus.service
-
-# chmod 644 /usr/lib/systemd/system/nexus.service
-# restorecon  /usr/lib/systemd/system/nexus.service
-
-# systemctl daemon-reload
-# systemctl start nexus.service
-# systemctl enable nexus.service
-# systemctl status --no-pager nexus.service
-# echo "The container is set to automatically start with the operating system."
